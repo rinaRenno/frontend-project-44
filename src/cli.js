1aeb4aaaa591cafd44d3060378406688fe6b67fa
rinaRenno <125858988+rinaRenno@users.noreply.github.com>
2023-03-20 08:53:45 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -5,5 +5,5 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
-  return console.log(`Hello, ${username}!`);
+  return `Hello, ${username}!`;
 };
